c09243e3ee47374416de1bd1f0d12b6e55442a82
typeOfMango <23davewood@gmail.com>
2026-02-19 17:22:54 +0000

    part 2 done

--- part2.py
+++ part2.py
@@ -6,6 +6,9 @@ searchNumber = randint(1,50)
 for i in range(howMany):
     randomList.append(randint(1,50))
 
-print("Generated list: "randomList)
-if searchNumber in randomList
-    print("The number",)
+print("Generated list: ",randomList)
+print("Searching for number",str(searchNumber)+"...")
+if searchNumber in randomList:
+    print("The number", searchNumber,"was found!!!!!")
+else:
+    print("The number",searchNumber,"was not found :(")
