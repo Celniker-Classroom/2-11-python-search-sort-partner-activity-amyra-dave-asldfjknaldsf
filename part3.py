f696862581d2f54cf398c0f2907254331e876eb0
typeOfMango <23davewood@gmail.com>
2026-02-20 16:34:38 +0000

    finished part 3 probably

--- part3.py
+++ part3.py
@@ -1,9 +1,14 @@
 #add randint
-howMany = 10
-rangeLength = 50
 from random import randint
+#how many numbers in the list
+howMany = randint(6,17)
+#the range that random numbers can be generated in from 1-x
+rangeLength = randint(20, 60)
+#make empty lists
 randomList = []
 cubedList = []
+evens = 0
+odds = 0
 #searchNumber = int(input("Choose a number to search for: "))
 #comparisons = 0
 #found = False
@@ -11,15 +16,22 @@ cubedList = []
 #create the random list
 for i in range(howMany):
     randomList.append(randint(1,rangeLength))
+print(f"The list has {howMany} numbers in it")
 print("the list is",randomList)
 print()
 sortedList = sorted(randomList)
 
 #print(f"searching for number {searchNumber}")
 #print("the list, sorted, is", sortedList)
+#iterates through the list of numbers
 for i in range(len(randomList)):
     #cubes the list
     cubedList.append(randomList[i]**3)
+    #counts evens and odds
+    if randomList[i] % 2 == 0:
+        evens += 1
+    else:
+        odds += 1
     #comparisons += 1
     #currentSum += int(randomList[i])
     '''if randomList[i] == searchNumber:
@@ -33,3 +45,4 @@ sortedList.reverse()
 print("the list in reverse order from sorted is", sortedList)
 
 #print(f"the sum is {currentSum}")
+print(f"There are {odds} odd numbers and {evens} even numbers")
