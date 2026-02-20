893903a1737dc2b7157dcf646eefd020a03f1295
typeOfMango <23davewood@gmail.com>
2026-02-20 16:55:26 +0000

    partway through cleaning up pt3

--- part3.py
+++ part3.py
@@ -1,3 +1,7 @@
+toDo = input("What do you want to do\n" 
+"1. Generate a random list and search for a number\n" 
+"2. Do three silly things to the list\n" \
+"")
 #add randint
 from random import randint
 #how many numbers in the list
@@ -7,42 +11,41 @@ rangeLength = randint(20, 60)
 #make empty lists
 randomList = []
 cubedList = []
-evens = 0
-odds = 0
-#searchNumber = int(input("Choose a number to search for: "))
-#comparisons = 0
-#found = False
-#currentSum = 0
-#create the random list
-for i in range(howMany):
-    randomList.append(randint(1,rangeLength))
-print(f"The list has {howMany} numbers in it")
-print("the list is",randomList)
-print()
-sortedList = sorted(randomList)
+if toDo == 1:
+    searchNumber = int(input("Choose a number to search for: "))
+    comparisons = 0
+    found = False
+    #create the random list
+    for i in range(howMany):
+        randomList.append(randint(1,rangeLength))
+    print(f"The list has {howMany} numbers in it")
+    print("the list is",randomList)
+    print()
+    print(f"searching for number {searchNumber}")
+    #iterates through the list to see if the searchNumber is in the list
+    for i in range(len(randomList)):
+        comparisons += 1
+        if randomList[i] == searchNumber:
+            found = True
+            print(f"I found the number {searchNumber}!!!")
+            print("comparisons:", comparisons)
+            break
 
-#print(f"searching for number {searchNumber}")
-#print("the list, sorted, is", sortedList)
-#iterates through the list of numbers
-for i in range(len(randomList)):
-    #cubes the list
-    cubedList.append(randomList[i]**3)
-    #counts evens and odds
-    if randomList[i] % 2 == 0:
-        evens += 1
-    else:
-        odds += 1
-    #comparisons += 1
-    #currentSum += int(randomList[i])
-    '''if randomList[i] == searchNumber:
-        found = True
-        print(f"I found the number {searchNumber}!!!")
-        print("comparisons:", comparisons)
-        break
-        '''
-print("The list cubed is",cubedList)
-sortedList.reverse()
-print("the list in reverse order from sorted is", sortedList)
-
-#print(f"the sum is {currentSum}")
-print(f"There are {odds} odd numbers and {evens} even numbers")
+elif toDo == 2:
+    evens = 0
+    odds = 0
+    sortedList = sorted(randomList)
+    print("the list, sorted, is", sortedList)
+    #iterates through the list of numbers
+    for i in range(len(randomList)):
+        #cubes the list
+        cubedList.append(randomList[i]**3)
+        #counts evens and odds
+        if randomList[i] % 2 == 0:
+            evens += 1
+        else:
+            odds += 1
+    print("The list cubed is",cubedList)
+    sortedList.reverse()
+    print("the list in reverse order from sorted is", sortedList)
+    print(f"There are {odds} odd numbers and {evens} even numbers")
